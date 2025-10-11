fff2e4220084245add2bd96316c5fdd26a571a86
Zeki Can EKŞİ <zekicaneksi@gmail.com>
2025-10-11 13:33:34 +0300

    opening dialog window to choose a directory -- and a little styling and formatting

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+let SelectDirectory = () => {
+    window.electronAPI.SelectDirectory().then( (path : void): void | PromiseLike<void> => {
+        console.log(path)
+        if (path === undefined) {
+            console.log("dont open app")
+        } else {
+            console.log("open app")
+        }
+    })
+}
+
 const root = createRoot(document.body);
-root.render(<h2 className="text-lime-400">Hello from React!</h2>);
+root.render(
+    <div className="flex items-center justify-center h-screen">
+        <div className="flex flex-col items-center justify-center">
+            <h1 className="text-[#d85630] font-bold">Welcome!</h1>
+            <p className="text-[#d85630]">Please select your music directory</p>
+            <button onClick={SelectDirectory}>click me</button>
+        </div>
+    </div>
+);
--- frontend/src/preload.ts
+++ frontend/src/preload.ts
@@ -1,2 +1,8 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+import {contextBridge, ipcRenderer} from 'electron'
+
+contextBridge.exposeInMainWorld('electronAPI', {
+    SelectDirectory: () => ipcRenderer.invoke('dialog:openDirectory')
+})
