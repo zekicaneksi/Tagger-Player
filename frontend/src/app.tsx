d7d03c20a0cad4eef9819af66821a0268ac86503
Zeki Can EKŞİ <zekicaneksi@gmail.com>
2025-10-26 14:44:16 +0300

    annoying

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { createHashRouter } from "react-router";
-import { RouterProvider } from "react-router";
+import { createHashRouter, RouterProvider } from "react-router";
 
 import HomePage from "./home/page";
 
