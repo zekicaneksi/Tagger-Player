eada258abc544e424d0b9c296e575a44ef30ca1a
Zeki Can EKŞİ <zekicaneksi@gmail.com>
2025-11-22 11:02:17 +0300

    created a listbox component, and tested it

---
+++ frontend/src/components/Listbox.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useState, SetStateAction } from 'react';
+
+
+interface listboxElemProps {
+    index: number;
+    text: string;
+    isSelected: boolean;
+    setSelectedIndex: React.Dispatch<SetStateAction<number>>;
+}
+
+const ListboxElem = (props: listboxElemProps) => {
+    return(
+        <div className={`w-full border-b ${props.isSelected ? "bg-neutral-400" : "bg-neutral-500"} hover:bg-neutral-400 p-[3px]`} onClick={() => props.setSelectedIndex(props.index)}>
+        <p>{props.text}</p>
+        </div>
+    )
+}
+
+interface props {
+    elements: string[];
+    selectedIndex: number;
+    setSelectedIndex: React.Dispatch<SetStateAction<number>>;
+}
+
+const Listbox = (props:props) => {
+    return(
+        <div className="h-full w-full bg-neutral-600 flex flex-col border border-2 border-neutral-500 overflow-y-scroll wrap-break-word">
+        {props.elements.map((elem, index) => <ListboxElem key={index} index={index} text={elem} isSelected={props.selectedIndex === index ? true : false} setSelectedIndex={props.setSelectedIndex}/>)}
+        </div>
+    ) 
+}
+
+export default Listbox;
--- frontend/src/home/App.tsx
+++ frontend/src/home/App.tsx
@@ -1,14 +1,47 @@
 import React from 'react';
+import { useState } from 'react';
+
+import Listbox from '../components/Listbox'
 
 interface props {
     locationUuid: string
 }
 
 const App = (props: props) => {
+
+    const [tagListboxIndex, setTagListboxIndex] = useState<number>(-1);
+    
+    const elemArr = [
+        "oompa",
+        "lumpppa",
+        "dubidu",
+        "diip",
+        "give",
+        "me",
+        "chocolateeeeeeeee iiiiiiiiii ooooooooooooo hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh",
+        "cohocooooooooooooooooooooooooooooooooooooooooooos",
+        "please",
+        "willy",
+        "wonka",
+        "thankyou"
+    ]
+
     return (
+        <>
+        <div className="w-sm h-50">
+        <Listbox
+        elements={elemArr}
+        selectedIndex={tagListboxIndex}
+        setSelectedIndex={setTagListboxIndex}
+        /> 
+        </div>
+
+        <p>Selected element is: {tagListboxIndex === -1 ? "no element is selected" : elemArr[tagListboxIndex]}</p>
+
         <div>
         <p>Hello from app, uuid is: {props.locationUuid}</p>
         </div>
+        </>
     )
 }
 
